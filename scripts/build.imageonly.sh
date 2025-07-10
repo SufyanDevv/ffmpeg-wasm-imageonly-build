f7cc03fa1bde993610f965fb019d2c36574f57f8
SufyanDevv <maaroufsoufiane2003@gmail.com>
2025-07-10 23:16:00 +0100

    Update build.imageonly.sh

--- scripts/build.imageonly.sh
+++ scripts/build.imageonly.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
-EMSCRIPTEN_PATH=/emsdk/upstream/emscripten
-
-export PATH=$EMSCRIPTEN_PATH:$PATH
-
-FFMPEG_FLAGS="
-  --disable-everything
-  --enable-demuxer=png,jpeg,bmp,gif,webp
-  --enable-muxer=png,jpeg,bmp,gif,webp
-  --enable-decoder=png,jpeg,bmp,gif,webp
-  --enable-encoder=png,jpeg,bmp,gif,webp
-  --enable-protocol=file
-  --enable-filter=scale
-  --enable-avcodec
-  --enable-avformat
-  --enable-swscale
-"
-
-docker run --rm -v $(pwd):/build -w /build \
-  emscripten/emsdk bash -c "
+docker run --rm -v $(pwd):/build -w /build emscripten/emsdk bash -c "
   git clone https://github.com/FFmpeg/FFmpeg.git &&
   cd FFmpeg &&
-  ./configure --cc=emcc --target-os=none --arch=x86_32 --disable-x86asm \
-    $FFMPEG_FLAGS --prefix=/build/dist &&
+  emconfigure ./configure \
+    --cc=emcc \
+    --target-os=none \
+    --arch=x86_32 \
+    --disable-x86asm \
+    --disable-everything \
+    --enable-demuxer=png,jpeg,bmp,gif,webp \
+    --enable-muxer=png,jpeg,bmp,gif,webp \
+    --enable-decoder=png,jpeg,bmp,gif,webp \
+    --enable-encoder=png,jpeg,bmp,gif,webp \
+    --enable-protocol=file \
+    --enable-filter=scale \
+    --enable-avcodec \
+    --enable-avformat \
+    --enable-swscale \
+    --prefix=/build/dist &&
   make -j4 &&
   make install
 "
